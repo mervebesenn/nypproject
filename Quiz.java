edd4a8045b8ebe9104b888d1a52e0db45ceb3679
MerveBesen <61238263+MerveBesen@users.noreply.github.com>
2020-02-19 19:45:56 +0300

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,9 +1,17 @@
 package quiz;
 
 class user{
-    private String name;
+    private String username;
     private String mail;
     private String password;
+}
+
+class question{
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
 }
 
 public class Quiz {
